b041629cc3d79e065b0e0bbf70ca1885b19e0a7f
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-05-22 16:26:51 -0700

    created functions and varibales of mouse events and drawing

--- script.js
+++ script.js
@@ -4,11 +4,40 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
-// global variable for size & color
-let size = 20
+// global variable
+let size = 10
+let isPressed = false
 let color = 'black'
 let x
 let y
+
+// event listener
+canvas.addEventListener('mousedown', (e) => {
+  isPressed = true
+  // positions of mouse
+  x = e.offsetX
+  y = e.offsetY
+})
+
+document.addEventListener('mouseup', (e) => {
+  isPressed = false
+  // positions of mouse
+  x = undefined
+  y = undefined
+})
+
+canvas.addEventListener('mousemove', (e) => {
+  if (isPressed) {
+    // positions of mouse
+    const x2 = e.offsetX
+    const y2 = e.offsetY
+    drawCircle(x2, y2)
+    drawLine(x, y, x2, y2)
+    // update x and y
+    x = x2
+    y = y2
+  }
+})
 
 function drawCircle(x, y) {
   ctx.beginPath()
@@ -22,6 +51,6 @@ function drawLine(x1, y1, x2, y2) {
   ctx.moveTo(x1, y1) // Starting point
   ctx.lineTo(x2, y2) // End point
   ctx.strokeStyle = color
-  ctx.lineWidth = size
+  ctx.lineWidth = size * 2
   ctx.stroke()
 }
